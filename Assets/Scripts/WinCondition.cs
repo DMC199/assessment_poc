e9e9bb063664b4b907571a733d92db707115032d
Lance Booth <computerwizz98@yahoo.com>
2016-03-14 22:24:45 -0700

    Added the win condition checker. I honestly don't know if this is good Unity 'style', or how to make it work with the other Unity parts. I'm going to need help on that.

---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum gearSpeed
+{
+    fast, // Overdrive when used for output
+    normal, // Direct drive when used for output
+    slow, // Low gear when used for output
+    stationary, // Is not used for output
+    reverse // same name when used as output
+}
+
+public class WinCondition : MonoBehaviour {
+    public Animator gearAnimation;
+
+    public bool ConditionMet(gearSpeed desiredOutput, gearSpeed sunInput, gearSpeed ringInput)
+    {
+        gearSpeed desiredSunInput = gearSpeed.stationary; // Initialized because unity complains
+        gearSpeed desiredRingInput = gearSpeed.stationary;// when they aren't
+
+        switch(desiredOutput)
+        {
+        case gearSpeed.fast:
+            desiredSunInput = gearSpeed.normal;
+            desiredRingInput = gearSpeed.fast;
+            break;
+        case gearSpeed.normal:
+            desiredSunInput = gearSpeed.normal;
+            desiredRingInput = gearSpeed.normal;
+            break;
+        case gearSpeed.slow:
+            desiredSunInput = gearSpeed.normal;
+            desiredRingInput = gearSpeed.stationary;
+            break;
+        case gearSpeed.reverse:
+            desiredSunInput = gearSpeed.reverse;
+            desiredRingInput = gearSpeed.stationary;
+            break;
+        };
+
+        if (desiredSunInput == sunInput && desiredRingInput == ringInput)
+        {
+            //gearAnimation.Play() put animation state cooresponding to desiredOutput in arguments list
+            // Additionally, make the checkbox do it's animation
+            return true;
+        }
+        return false;
+    }
+}
