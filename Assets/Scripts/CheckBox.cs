e38ee1d767293a5ba27d5ae32e11d311b4f17088
Demetri V.S <demetriv.s.7@gmail.com>
2016-03-14 20:37:07 -0700

    Added the win sound as a component of the check box
    +++Added the win sound as a component of the check box
    +++Attempted to add a sketch for the animation
    >>> Attempt failed
    >>> Current sketch can be deleted (or replaced by Lance)
    
    ---Removed the standalone sound

---
+++ Assets/Scripts/CheckBox.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckBox : MonoBehaviour {
+
+	bool test = true;
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Qcorrect(test);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void Qcorrect(bool Win)
+	{
+
+		if (Win == true) 
+		{
+			anim.SetBool ("IsCorrect", true);
+		}
+	}
+
+
+
+}
